13be50ba24ec8922237dd2af80a3a2893dad1568
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-10-06 08:51:42 +0530

    Hashing password setup using bcrypt and login and signup page ready

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,19 @@ const userSchema = new mongoose.Schema({
         minlength: [6, "Length must be greated than 6"]
     }
 });
+
+// fire a function after doc saved to DB...
+//userSchema.post("save", function(doc, next){
+//    console.log(doc);
+//    next();
+//});
+
+// fire a function before doc saved to DB...
+userSchema.pre("save", async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
 const User = mongoose.model("user", userSchema);
 
